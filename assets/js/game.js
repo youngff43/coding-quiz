55fa414baeb249a37c1c81324378b46d541f7f0f
youngff43 <youngff43@gmail.com>
2022-03-23 18:16:40 -0400

    Added all questions, started function

---
+++ assets/js/game.js
@@ -0,0 +1,77 @@
+//Global Variables//
+
+var questionsEl = document.getElementById("questions");
+
+
+//Quiz Questions//
+var quizQuestions = [{
+    question: "What does HTML stand for?",
+    choiceA: "Hyperlinks and Text Markup Language",
+    choiceB: "Home Tool Markup Language",
+    choiceC: "Hyper Text Markup Language",
+    choiceD: "Hard Tools Mean Less",
+    corrrectAnswer: "C"},
+{
+    question: "What is the correct HTML element for the largest heading?",
+    choiceA: "<head>",
+    choiceB: "<header>",
+    choiceC: "<h6>",
+    choiceD: "<h1>",
+    corrrectAnswer: "D"},
+{
+    question: "What does CSS stand for?",
+    choiceA: "Computer Style Sheets",
+    choiceB: "Creative Style Sheets",
+    choiceC: "Cascading Style Sheets",
+    choiceD: "Colorful Style Sheets",
+    corrrectAnswer: "C"},
+{
+    question: "How is document type initialized in HTML5.?",
+    choiceA: "</DOCTYPE HTML>",
+    choiceB: "</DOCTYPE>",
+    choiceC: "<!DOCTYPE HTML>",
+    choiceD: "</DOCTYPE html>",
+    corrrectAnswer: "C"},
+{
+    question: "Which of the following HTML Elements is used for making any text bold ?",
+    choiceA: "<b>",
+    choiceB: "<p>",
+    choiceC: "<i>",
+    choiceD: "<li>",
+    corrrectAnswer: "A"},
+{
+    question: "Which of the following characters indicate closing of a tag?",
+    choiceA: ".",
+    choiceB: "/",
+    choiceC: "//",
+    choiceD: "!",
+    corrrectAnswer: "B"},
+{
+    question: "How many attributes are there in HTML5?",
+    choiceA: "1",
+    choiceB: "2",
+    choiceC: "3",
+    choiceD: "4",
+    corrrectAnswer: "B"},
+{
+    question: "Which of the following attributes is used to add a link to any element?",
+    choiceA: "link",
+    choiceB: "ref",
+    choiceC: "href",
+    choiceD: "<newref",
+    corrrectAnswer: "C"},
+{
+    question: "Whats the purpose of using div tags in HTML?",
+    choiceA: "for creating different styles",
+    choiceB: "for creating different sections",
+    choiceC: "for adding headings",
+    choiceD: "for adding titles",
+    corrrectAnswer: "B"},
+{
+    question: "Which of the following elements can be used in HTML to create a table?",
+    choiceA: "<table> , <tbody> , <trow>",
+    choiceB: "<table> , <tb> , <trow>",
+    choiceC: "<table> , <tbody> , <tr>",
+    choiceD: "All of the above",
+    corrrectAnswer: "D"},
+];
