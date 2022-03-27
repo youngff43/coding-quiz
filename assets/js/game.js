7abb91122d734e3f58a6c9c2820b7b14bc3b9a37
youngff43 <99932948+youngff43@users.noreply.github.com>
2022-03-27 00:15:46 -0400

    clean up functions and css

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,29 +1,23 @@
 //Global Variables//
 
 var questions = document.getElementById("questions");
-var lastQuestionIndex = quizQuestions.length;
-var currentQuestionIndex = 0;
 var buttonA = document.getElementById("a");
 var buttonB = document.getElementById("b");
 var buttonC = document.getElementById("c");
 var buttonD = document.getElementById("d");
 var endQuizDiv = document.getElementById("end-quiz");
-var startPageDiv = document.getElementById("front-page");
-var timerInterval; 
 var actualTimer = document.getElementById("timer");
-var totalTime = 60;
 var actualQuiz = document.getElementById("quiz");
 var highScoreInitials = document.getElementById("highscore-initials");
-var finalScore = document.getElementById("score");
+var endScore = document.getElementById("end-score");
 var startQuizButton = document.getElementById("start-button");
 var submitScoreButton = document.getElementById("submit-score");
-var correct;
-var score = 0;
 var overallHighscore = document.getElementById("overall-highscore");
 var totalPage = document.getElementById("total-page");
 var finalButtons = document.getElementById("final-buttons");
 var highScoresScore = document.getElementById("highscore-score");
 var beginningQuiz = document.getElementById("beginning-quiz");
+var answerResult = document.getElementById("answer-result");
 
 //Quiz Questions//
 var quizQuestions = [{
@@ -32,78 +26,84 @@ var quizQuestions = [{
     choiceB: "Home Tool Markup Language",
     choiceC: "Hyper Text Markup Language",
     choiceD: "Hard Tools Mean Less",
-    correctAnswer: "C"},
+    correctAnswer: "c"},
 {
     question: "What is the correct HTML element for the largest heading?",
-    choiceA: "<head>",
-    choiceB: "<header>",
-    choiceC: "<h6>",
-    choiceD: "<h1>",
-    correctAnswer: "D"},
+    choiceA: 'head',
+    choiceB: "header",
+    choiceC: "h6",
+    choiceD: "h1",
+    correctAnswer: "d"},
 {
     question: "What does CSS stand for?",
     choiceA: "Computer Style Sheets",
     choiceB: "Creative Style Sheets",
     choiceC: "Cascading Style Sheets",
     choiceD: "Colorful Style Sheets",
-    correctAnswer: "C"},
+    correctAnswer: "c"},
 {
     question: "How is document type initialized in HTML5.?",
-    choiceA: "</DOCTYPE HTML>",
-    choiceB: "</DOCTYPE>",
-    choiceC: "<!DOCTYPE HTML>",
-    choiceD: "</DOCTYPE html>",
-    correctAnswer: "C"},
+    choiceA: "/DOCTYPE HTML",
+    choiceB: "/DOCTYPE",
+    choiceC: "!DOCTYPE HTML",
+    choiceD: "/DOCTYPE html",
+    correctAnswer: "c"},
 {
     question: "Which of the following HTML Elements is used for making any text bold ?",
-    choiceA: "<b>",
-    choiceB: "<p>",
-    choiceC: "<i>",
-    choiceD: "<li>",
-    correctAnswer: "A"},
+    choiceA: "b",
+    choiceB: "p",
+    choiceC: "i",
+    choiceD: "li",
+    correctAnswer: "a"},
 {
     question: "Which of the following characters indicate closing of a tag?",
     choiceA: ".",
     choiceB: "/",
     choiceC: "//",
     choiceD: "!",
-    correctAnswer: "B"},
+    correctAnswer: "b"},
 {
     question: "How many attributes are there in HTML5?",
     choiceA: "1",
     choiceB: "2",
     choiceC: "3",
     choiceD: "4",
-    correctAnswer: "B"},
+    correctAnswer: "b"},
 {
     question: "Which of the following attributes is used to add a link to any element?",
     choiceA: "link",
     choiceB: "ref",
     choiceC: "href",
-    choiceD: "<newref",
-    correctAnswer: "C"},
+    choiceD: "newref",
+    correctAnswer: "c"},
 {
     question: "Whats the purpose of using div tags in HTML?",
     choiceA: "for creating different styles",
     choiceB: "for creating different sections",
     choiceC: "for adding headings",
     choiceD: "for adding titles",
-    correctAnswer: "B"},
+    correctAnswer: "b"},
 {
     question: "Which of the following elements can be used in HTML to create a table?",
     choiceA: "<table> , <tbody> , <trow>",
     choiceB: "<table> , <tb> , <trow>",
     choiceC: "<table> , <tbody> , <tr>",
     choiceD: "All of the above",
-    correctAnswer: "D"},
+    correctAnswer: "d"},
 ];
 
+var lastQuestionIndex = quizQuestions.length;
+var currentQuestionIndex = 0;
+var totalTime = 75;
+var timerInterval; 
+var correct;
+var score = 0;
 //Sets up the questions and answers//
 
 function generateQuizQuestion(){
     endQuizDiv.style.display = "none";
     if (currentQuestionIndex === lastQuestionIndex){
-        return endScore();
+        return showScore();
     }
     var currentQuestion = quizQuestions[currentQuestionIndex];
     questions.innerHTML = "<p>" + currentQuestion.question + "</p>";
@@ -117,7 +117,8 @@ function generateQuizQuestion(){
 
 function startQuiz(){
     endQuizDiv.style.display = "none";
-    startPageDiv.style.display = "none";
+    beginningQuiz.style.display = "none";
+    actualQuiz.style.display ="block";
     generateQuizQuestion();
     
 //Starting the timer//
@@ -128,25 +129,24 @@ timerInterval = setInterval(function() {
 
     if(totalTime === 0) {
         clearInterval(timerInterval);
-        endScore();
+        showScore();
     }
 }, 1000);
-actualQuiz.style.display ="block";
 }
 
 //Showing the end score//
 
-function endScore(){
+function showScore(){
     actualQuiz.style.display = "none"
     endQuizDiv.style.display = "flex";
     clearInterval(timerInterval);
     highScoreInitials.value = "";
-    finalScore.innerHTML = "Your final score is " + score;
+    endScore.innerHTML = "Your final score is " + score;
 }
 
 submitScoreButton.addEventListener("click", function highScore(){
 
-    if(highScoreInitials.value === "") {
+    if(highScoreInitials.value === " ") {
         alert("Cannot be blank");
         return false;
     }else{
@@ -163,7 +163,7 @@ submitScoreButton.addEventListener("click", function highScore(){
     finalButtons.style.display = "flex";
 
     savedHighscores.push(currentHighScore);
-    localStorage.setItem("savedHighScores", JSON.stringify(savedHighscores));
+    localStorage.setItem("savedHighscores", JSON.stringify(savedHighscores));
     showHighscores();
 
 }
@@ -173,11 +173,11 @@ submitScoreButton.addEventListener("click", function highScore(){
 //Makes the high scores//
 
 function showHighscores(){
-    highScoreInitials.innerHTML = "";
-    highScoresScore.innerHTML = "";
-    var allScores = JSON.parse(localStorage.getItem("savedHighScores")) || [];
+    highScoreInitials.innerHTML = " ";
+    highScoresScore.innerHTML = " ";
+    var allScores = JSON.parse(localStorage.getItem("savedHighscores")) || [];
     for (i=0; i<allScores.length; i++) {
-        var currentPlayer = document.createElement("li");
+        var currentPlayer = document.createElement("p");
         var currentScore = document.createElement("li");
         currentPlayer.textContent = allScores[i].name;
         currentScore.textContent = allScores[i].score;
@@ -192,37 +192,50 @@ function viewScore(){
     overallHighscore.style.display = "flex";
     totalPage.style.display = "block";
     finalButtons.style.display = "flex";
-
     showHighscores();
+}
+
+//Clearing the table//
+
+function clearTable(){
+    window.localStorage.clear();
+    highScoreInitials.textContent = " ";
+    highScoresScore.textContent = " ";
 }
 
 //Checking answers//
 
-function clearTable(){
-    window.localStorage.clear();
-    highScoreInitials.textContent = "";
-    highScoresScore.textContent = "";
-}
-
 function checkAnswer(answer){
     correct = quizQuestions[currentQuestionIndex].correctAnswer;
 
+    answerResult.style.display = "block";
+    var result = document.createElement("result");
+    answerResult.appendChild(result);
+    setTimeout(function () {
+        result.style.display = "none";
+    }, 1000);
+
     if (answer === correct && currentQuestionIndex !== lastQuestionIndex){
         score++;
-        alert("Correct!");
+        result.textContent = "You are Correct!";
         currentQuestionIndex++;
         generateQuizQuestion();
     }else if (answer !== correct && currentQuestionIndex !== lastQuestionIndex){
-        alert("Wrong!");
+        result.textContent = "Wrong!";
         currentQuestionIndex++;
+        totalTime = totalTime - 5;
         generateQuizQuestion();
     }else{
-        endScore();
+        showScore();
     }
-
-//Clearing the table//
 }
-
-
+function replayQuiz() {
+    overallHighscore.style.display = "none";
+    endQuizDiv.style.display = "none";
+    beginningQuiz.style.display = "flex"
+    score = 0;
+    currentQuestionIndex = 0;
+    totalTime = 75;
+}
 
 startQuizButton.addEventListener("click",startQuiz);
